c792c1f6346f4351437c2fc6f976eb18ac4c1d03
André Simões <afsdede92@gmail.com>
2013-04-08 23:34:14 -0300

    Perfil

---
+++ perfil.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'bootstrap.php');
+
+use KernelEngine\KernelEngine;
+use UserBundle\Entity\User;
+use UserBundle\Controller\UserController;
+use KernelBundle\Model\Entity;
+
+class mainExecution extends KernelEngine {
+
+    function __construct() {
+        parent::__construct();
+
+
+        if (!isset($_SESSION['userLogin'])) {
+            header('Location: login.php');
+        }
+    }
+
+    function fixObject(&$object) {
+        if (!is_object($object) && gettype($object) == 'object')
+            return ($object = unserialize(serialize($object)));
+        return $object;
+    }
+
+}
+
+$a = new mainExecution();
+
+$usrController = new UserController();
+
+if (!isset($_GET)){
+
+    echo $usrController->indexAction($a->fixObject($_SESSION['userLogin']));
+
+}else {
+    
+    if (isset($_GET['cliId'])){
+        
+        echo $usrController->indexClienteAction($a->fixObject($_SESSION['userLogin']));
+        
+    }
+    
+}
+?>
