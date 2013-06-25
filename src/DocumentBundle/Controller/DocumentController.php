ccdcb8cdb8d1c09e659c1dbaaf82fddec3f4076c
André Simões <afsdede92@gmail.com>
2013-06-25 02:32:52 -0300

    Continuação da listagem de documentos

--- src/DocumentBundle/Controller/DocumentController.php
+++ src/DocumentBundle/Controller/DocumentController.php
@@ -105,7 +105,7 @@ class DocumentController extends Controller {
         $cat = new Categoria();
 
         if ($idCategory){
-            $idCategory = $id;
+            $idCategory = $idCategory;
         }else {
             $idCategory = '0';
         }
@@ -122,10 +122,54 @@ class DocumentController extends Controller {
             $newDoc = new Document();
             $newDoc->fetchEntity($vDoc);
             $docYear[] = date("Y",$newDoc->getDate());
-            $docRet[] = $newDoc;
         }
         $template = $indexView->getTemplate();
-        return $template->render('/src/UserBundle/View/src/index.html', array('nome' => $user->getNome(), 'user' => $user, "docYear" => array_unique($docYear)));
+        return $template->render('/src/DocumentBundle/View/src/listar-documento-categoria.html', array('nome' => $user->getNome(), 'user' => $user, 'catId' => $idCategory ,"docYear" => array_unique($docYear)));
+    }
+    
+    public function listarDocumentoCategoriaAnoAction(User $user, $idCategory, $ano) {
+        $indexView = new DocumentView();
+        $docList = array();
+
+        if ($idCategory){
+            $idCategory = $idCategory;
+        }else {
+            $idCategory = '0';
+        }
+        
+        $inicioData = mktime(0, 0, 1, 1, 1, $ano);
+        $finalData = mktime(24, 60, 0, 12, 31, $ano);
+        
+        $critDocuments = array(
+            'cat_10_id' => $idCategory,
+            'doc_10_data' => array(
+                'operator' => '>',
+                'val' => $inicioData
+            ),
+            'doc_10_data' => array(
+                'operator' => '<',
+                'val' => $finalData
+            )
+        );
+        $docRet = array();
+        $docYear = array();
+
+        $doc = new Document();
+        $docController = new DocumentController();
+        $docList = $docController->listAction($doc, "", $critDocuments);
+        $cliList = array();
+        foreach($docList as $kDoc => $vDoc){
+            $newDoc = new Document();
+            $newDoc->fetchEntity($vDoc);
+            $cli = new Cliente();
+            $cliController = new ClienteController();
+            if (!in_array($newDoc->getClient(), $cliList)){
+                $cliRet = $cliController->listAction($cli, $newDoc->getClient());
+                $cliList[$newDoc->getClient()] = $cli->fetchEntity($cliRet[1]);
+            }
+        }
+        $template = $indexView->getTemplate();
+        return $template->render('/src/DocumentBundle/View/src/listar-documento-categoria-ano.html', array('nome' => $user->getNome(), 'user' => $user, 'catId' => $idCategory ,"docYear" => $ano, 'listCli' => $cliList));
     }
 
     public function editarAction(User $user, Document $doc) {
